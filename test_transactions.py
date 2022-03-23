72ac256a5322f4c8272c661b16da9f88c91df3d2
jameskong098 <jameskong098@gmail.com>
2022-03-23 15:23:43 -0400

    worked on transactions and pytest

--- test_transactions.py
+++ test_transactions.py
@@ -9,3 +9,18 @@ def trackerDB():
     ''' create tracker database '''
     db = transactions(db)
     yield db
+
+#Completed By James Kong on 3/22/2022
+@pytest.fixture
+def testShowTransactions():
+    db = "tracker.db"
+    transaction1 = {'item #': 3,'amount': 30, 'category': 'gaming', 'date': '03/23/22'}
+    transaction2 = {'item #': 5,'amount': 20, 'category': 'pet supplies', 'date': '03/24/22'}
+    transaction3 = {'item #': 2,'amount': 96, 'category': 'toiletries', 'date': '03/25/22'}
+    id1= db.add(transaction1)
+    id2= db.add(transaction2)
+    id3= db.add(transaction3)
+    yield db
+    db.delete(id3)
+    db.delete(id2)
+    db.delete(id1)
